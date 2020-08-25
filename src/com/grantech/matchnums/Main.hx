da20061f4e4b7779d67e33a9a042244adbde5ce5
Mansour <mansurjavadi@gmail.com>
2020-08-25 10:32:18 +0430

    Add primary methods

--- src/com/grantech/matchnums/Main.hx
+++ src/com/grantech/matchnums/Main.hx
@@ -1,8 +1,27 @@
 package com.grantech.matchnums;
 import openfl.display.Sprite;
+import openfl.events.Event;
 class Main extends Sprite {
 	public function new() {
 		super();
 
+		this.initialize();
+		this.construct();
+
+		this.resize(stage.stageWidth, stage.stageHeight);
+		stage.addEventListener(Event.RESIZE, this.stage_resizeHandler);
+	}
+
+	private function construct():Void {
+	}
+
+	private function initialize():Void {
+	}
+
+	private function resize(newWidth:Int, newHeight:Int):Void {
+	}
+
+	private function stage_resizeHandler(event:Event):Void {
+		this.resize(stage.stageWidth, stage.stageHeight);
 	}
 }
