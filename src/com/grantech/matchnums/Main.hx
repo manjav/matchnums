4b0cf4ea96ba7bd4dce06c2fdcb2bc954784eed9
Mansour <mansurjavadi@gmail.com>
2020-08-25 10:33:27 +0430

    Add header and footer

--- src/com/grantech/matchnums/Main.hx
+++ src/com/grantech/matchnums/Main.hx
@@ -1,7 +1,14 @@
 package com.grantech.matchnums;
+
+import openfl.Assets;
+import openfl.display.Bitmap;
 import openfl.display.Sprite;
 import openfl.events.Event;
+
 class Main extends Sprite {
+	private var background:Bitmap;
+	private var footer:Bitmap;
+
 	public function new() {
 		super();
 
@@ -13,12 +20,25 @@ class Main extends Sprite {
 	}
 
 	private function construct():Void {
+		this.footer.smoothing = true;
+
+		this.addChild(this.background);
+		this.addChild(this.footer);
 	}
 
 	private function initialize():Void {
+		this.background = new Bitmap(Assets.getBitmapData("images/background_tile.png"));
+		this.footer = new Bitmap(Assets.getBitmapData("images/center_bottom.png"));
 	}
 
 	private function resize(newWidth:Int, newHeight:Int):Void {
+		this.background.width = newWidth;
+		this.background.height = newHeight;
+
+		this.footer.scaleX = this.game.currentScale;
+		this.footer.scaleY = this.game.currentScale;
+		this.footer.x = (newWidth - this.footer.width) * 0.5;
+		this.footer.y = newHeight - this.footer.height;
 	}
 
 	private function stage_resizeHandler(event:Event):Void {
