93471fc7cc56f719db69f7b131553bc8073bdbe6
Mansour <mansurjavadi@gmail.com>
2020-08-25 10:35:15 +0430

    Add Game class

---
+++ src/com/grantech/matchnums/Game.hx
@@ -0,0 +1,18 @@
+package com.grantech.matchnums;
+import openfl.display.Sprite;
+
+class Game extends Sprite {
+	public function new() {
+		super();
+
+		var background = new Sprite();
+		background.graphics.beginFill(0xFFFFFF, 0.4);
+		background.graphics.drawRect(0, 0, 176 * NUM_COLUMNS, 176 * NUM_ROWS);
+		// background.filters = [new BlurFilter(10, 10)];
+		this.addChild(background);
+
+    }
+
+	public function resize(newWidth:Int, newHeight:Int):Void {
+	}
+}
