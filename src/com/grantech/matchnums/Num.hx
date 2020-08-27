935517f1e5be7e28e569c7c15dce2867ce6c5e21
Mansour <mansurjavadi@gmail.com>
2020-08-27 13:06:31 +0430

    Add text for num

--- src/com/grantech/matchnums/Num.hx
+++ src/com/grantech/matchnums/Num.hx
@@ -4,10 +4,14 @@ import openfl.geom.ColorTransform;
 import openfl.Assets;
 import openfl.display.Bitmap;
 import openfl.display.Sprite;
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+
 class Num extends Sprite {
-    static private final COLORS = [
+	static private final COLORS = [
 		0x000000, 0xFF00FF, 0x67d144, 0x44cac9, 0x3c85d6, 0xe06149, 0xaa00FF, 0x958a81, 0xffab36, 0xffab36, 0xffab36, 0xffab36, 0xffab36
 	];
+
 	public var column:Int;
 	public var row:Int;
 	public var value:Int;
@@ -15,11 +19,21 @@ class Num extends Sprite {
 	public function new(column:Int, row:Int, value:Int) {
 		super();
 
-        var color = new ColorTransform();
-        color.color = COLORS[value];
-        var background = new Bitmap(Assets.getBitmapData("images/tile.png"));
-        background.transform.colorTransform = color;
+		var color = new ColorTransform();
+		color.color = COLORS[value];
+		var background = new Bitmap(Assets.getBitmapData("images/tile.png"));
+		background.transform.colorTransform = color;
 		background.smoothing = true;
 		this.addChild(background);
+
+		var textDisplay = new TextField();
+		textDisplay.autoSize = CENTER;
+		textDisplay.selectable = false;
+		textDisplay.text = Math.pow(2, value) + "";
+		textDisplay.embedFonts = true;
+		textDisplay.defaultTextFormat = new TextFormat("Arial Rounded MT Bold", 72, 0xFFFFFF, true);
+		textDisplay.x = (this.width - textDisplay.width) * 0.5;
+		textDisplay.y = (this.height - textDisplay.height) * 0.5;
+		this.addChild(textDisplay);
 	}
 }
