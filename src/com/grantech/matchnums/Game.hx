02a049cc692a0040733d19f7b77e22e3e2e26495
Mansour <mansurjavadi@gmail.com>
2020-08-31 10:23:48 +0430

    Change inital x position

--- src/com/grantech/matchnums/Game.hx
+++ src/com/grantech/matchnums/Game.hx
@@ -1,6 +1,5 @@
 package com.grantech.matchnums;
 
-import motion.easing.Back;
 import motion.easing.Linear;
 import motion.easing.Bounce;
 import motion.Actuate;
@@ -9,7 +8,7 @@ import openfl.display.Sprite;
 
 class Game extends Sprite {
 	private static var NUM_COLUMNS = 5;
-	private static var NUM_ROWS = 6;
+	private static var NUM_ROWS = 7;
 	private static var CELL_SIZE = 176;
 
 	public var currentScale:Float = 1;
@@ -22,7 +21,7 @@ class Game extends Sprite {
 
 		var background = new Sprite();
 		background.graphics.beginFill(0xFFFFFF, 0.4);
-		background.graphics.drawRect(0, 0, CELL_SIZE * NUM_COLUMNS, CELL_SIZE * (NUM_ROWS + 1));
+		background.graphics.drawRect(0, 0, CELL_SIZE * NUM_COLUMNS, CELL_SIZE * NUM_ROWS);
 		// background.filters = [new BlurFilter(10, 10)];
 		this.addChild(background);
 
@@ -41,19 +40,19 @@ class Game extends Sprite {
 
 				var target = this.cells[f.column].length;
 				Actuate.stop(f);
-				Actuate.tween(f, 0.5, {y: CELL_SIZE * (NUM_ROWS - target)}).ease(Back.easeOut);
+				Actuate.tween(f, 0.5, {y: CELL_SIZE * (NUM_ROWS - target)}).ease(Bounce.easeOut);
 				f.row = target;
 				this.cells[f.column].push(f);
 			}
 			return;
 		}
 
-		var column = Math.floor(Math.random() * NUM_COLUMNS);
-		var cell = Cell.instantiate(Math.floor(Math.random() * NUM_COLUMNS), -1, column);
+		var column = Math.floor(event.localX / CELL_SIZE);
+		var cell = Cell.instantiate(column, -1, Math.ceil(Math.random() * 8));
 		cell.x = column * CELL_SIZE;
 		this.floatings.push(cell);
 		var distance = CELL_SIZE * (NUM_ROWS - cells.length);
-		Actuate.tween(cell, distance * 0.03, {y: distance}).ease(Linear.easeNone);
+		Actuate.tween(cell, distance * 0.005, {y: distance}).ease(Linear.easeNone);
 		this.addChild(cell);
 	}
 
