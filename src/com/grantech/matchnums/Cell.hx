e5f42ddb39b66ff94891280f241007e16f49c234
Mansour <mansurjavadi@gmail.com>
2020-09-21 13:28:00 +0330

    Renwe cells color

--- src/com/grantech/matchnums/Cell.hx
+++ src/com/grantech/matchnums/Cell.hx
@@ -1,5 +1,12 @@
 package com.grantech.matchnums;
 
+import openfl.Assets;
+import openfl.display.BitmapData;
+import openfl.display.Shape;
+import openfl.display.Sprite;
+import openfl.filters.GlowFilter;
+import openfl.geom.ColorTransform;
+import openfl.text.AntiAliasType;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 
@@ -15,7 +22,7 @@ class Cell extends Sprite {
 	static public final BORDER = 8;
 	static public final ROUND = 32;
 	static private final COLORS = [
-		0x000000, 0xFF00FF, 0x67d144, 0x44cac9, 0x3c85d6, 0xe06149, 0xaa00FF, 0x958a81, 0xffab36, 0xfc5372
+		0x000000, 0x9600ff, 0xf0145a, 0xffc91b, 0x00c419, 0x009ade, 0xce007b4, 0xff5518, 0x78e0bc, 0x3c14ae, 0xff0024, 0x41046a, 0x41046a, 0x41046a, 0x41046a
 	];
 	static private final TEXT_SCALE = [1, 1, 1, 1, 0.9, 0.9, 0.9, 0.8, 0.8, 0.8, 0.6, 0.6, 0.6];
 
@@ -52,6 +59,28 @@ class Cell extends Sprite {
 			this.background.graphics.clear();
 		this.background.graphics.beginFill(0xFFFFFF);
 		this.background.graphics.drawRoundRect(BORDER, BORDER, SIZE - BORDER * 2, SIZE - BORDER * 2, ROUND, ROUND);
+
+		// var sg = new Rectangle(7, 7, 2, 2);
+		// var bd:BitmapData = Assets.getBitmapData("images/" + (this.value < 10 ? "tile" : Std.string(this.value)) + ".png");
+		// if (bd == null)
+		// 	return;
+		// var cols:Array<Float> = [sg.left, sg.right, bd.width];
+		// var rows:Array<Float> = [sg.top, sg.bottom, bd.height];
+		// var left:Float = 0;
+		// for (i in 0...3) {
+		// 	var top:Float = 0;
+		// 	for (j in 0...3) {
+		// 		// trace(left, top, cols[i] - left, rows[j] - top);
+		// 		this.background.graphics.beginBitmapFill(bd);
+		// 		this.background.graphics.drawRect(left, top, cols[i] - left, rows[j] - top);
+		// 		this.background.graphics.endFill();
+		// 		top = rows[j];
+		// 	}
+		// 	left = cols[i];
+		// }
+		// this.background.scale9Grid = sg;
+		// this.background.width = SIZE;
+		// this.background.height = SIZE;
 		this.addChildAt(this.background, 0);
 	}
 
@@ -66,7 +95,7 @@ class Cell extends Sprite {
 			createBackground();
 
 		var color = new ColorTransform();
-			color.color = COLORS[value];
+		color.color = COLORS[value];
 		this.background.transform.colorTransform = color;
 
 		this.textDisplay.text = Math.pow(2, value) + "";
