e917c22bea72113e0bc688e0aa0bba16e819c158
Mansour <mansurjavadi@gmail.com>
2020-09-28 11:11:27 +0330

    Add autosize to textcreator

--- src/com/grantech/matchnums/utils/Utils.hx
+++ src/com/grantech/matchnums/utils/Utils.hx
@@ -1,12 +1,14 @@
 package com.grantech.matchnums.utils;
 
+import openfl.text.TextFieldAutoSize;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 
 class Utils {
-	static public function createText(size:Int = 80, color:UInt = 0xFFFFFF, align:String = "center"):TextField {
+	static public function createText(size:Int = 80, color:UInt = 0xFFFFFF, align:String = "center", autoSize:TextFieldAutoSize = TextFieldAutoSize.CENTER):TextField {
 		var textField = new TextField();
 		// textField.antiAliasType = AntiAliasType.ADVANCED;
+		textField.autoSize = autoSize;
 		textField.mouseEnabled = false;
 		textField.selectable = false;
 		textField.embedFonts = true;
