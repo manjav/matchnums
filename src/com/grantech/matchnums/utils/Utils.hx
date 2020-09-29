e15a83fe1289ef8bacaacd85cefc9d5c79b53dc8
Mansour <mansurjavadi@gmail.com>
2020-09-30 03:27:12 +0330

    Extract attribute-set for text

--- src/com/grantech/matchnums/utils/Utils.hx
+++ src/com/grantech/matchnums/utils/Utils.hx
@@ -1,12 +1,19 @@
 package com.grantech.matchnums.utils;
 
-import openfl.text.TextFieldAutoSize;
 import openfl.text.TextField;
+import openfl.text.TextFieldAutoSize;
 import openfl.text.TextFormat;
 
 class Utils {
-	static public function createText(size:Int = 80, color:UInt = 0xFFFFFF, align:String = "center", autoSize:TextFieldAutoSize = TextFieldAutoSize.CENTER):TextField {
+	static public function createText(size:Int = 80, color:UInt = 0xFFFFFF, align:String = "center",
+			autoSize:TextFieldAutoSize = TextFieldAutoSize.CENTER):TextField {
 		var textField = new TextField();
+		setTextAttributes(textField, size, color, align, autoSize);
+		return textField;
+	}
+
+	static public function setTextAttributes(textField:TextField, size:Int = 80, color:UInt = 0xFFFFFF, align:String = "center",
+			autoSize:TextFieldAutoSize = TextFieldAutoSize.CENTER):Void {
 		// textField.antiAliasType = AntiAliasType.ADVANCED;
 		textField.autoSize = autoSize;
 		textField.mouseEnabled = false;
@@ -14,6 +21,5 @@ class Utils {
 		textField.embedFonts = true;
 		textField.defaultTextFormat = new TextFormat("Arial Rounded MT Bold", size, color, null, null, null, null, null, align);
 		// textField.filters = [new GlowFilter(0, 0.6, 4, 4)];
-		return textField;
 	}
 }
