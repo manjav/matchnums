8fbe4b0dec19a4032b006b2718a0ad4649846c96
Mansour <mansurjavadi@gmail.com>
2020-10-15 16:21:22 +0330

    Add waiting

--- src/com/grantech/matchnums/Game.hx
+++ src/com/grantech/matchnums/Game.hx
@@ -286,6 +286,12 @@ class Game extends Sprite {
 				this.maxValue = cell.value - distance;
 		}
 		this.fallAll(false);
+
+		this.state = WaitForMerge;
+		this.timer = Timer.delay(function() {
+			this.state = Play;
+			this.fallAll(false);
+		}, 200);
 	}
 
 	public function resize(newWidth:Int, newHeight:Int):Void {
