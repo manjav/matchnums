e1e4e3010c37dc125c137b667e6aa9fe14d51057
Mansour <mansurjavadi@gmail.com>
2020-10-28 20:49:00 +0330

    Auto addChild screen

--- src/com/grantech/matchnums/screens/BaseScreen.hx
+++ src/com/grantech/matchnums/screens/BaseScreen.hx
@@ -1,18 +1,24 @@
+package com.grantech.matchnums.screens;
+
 import feathers.controls.LayoutGroup;
 import feathers.layout.AnchorLayoutData;
+import openfl.display.DisplayObjectContainer;
 enum ScreenType {
 	Pause;
 	Ads;
 }
 
 class BaseScreen extends LayoutGroup {
-	static public function create(screenType:ScreenType):BaseScreen {
+	static public function create(screenType:ScreenType, ?parent:DisplayObjectContainer):BaseScreen {
 		var screen = switch (screenType) {
 			case Pause:
 				new PuaseScreen();
 			default: null;
 		}
 		screen.layoutData = new AnchorLayoutData(0, 0, 0, 0);
+		if (parent != null)
+			parent.addChild(screen);
 		return screen;
 	}
 
+}
