6601458d7b461c1ffab4da92eb4287930be7ea8b
Mansour <mansurjavadi@gmail.com>
2020-11-05 13:41:21 +0330

    Move popups to dedicate package

--- src/com/grantech/matchnums/display/popups/BasePopup.hx
+++ src/com/grantech/matchnums/display/popups/BasePopup.hx
@@ -1,14 +1,18 @@
-package com.grantech.matchnums.display;
+package com.grantech.matchnums.display.popups;
 
+import com.grantech.matchnums.themes.OutlineTheme;
+import feathers.controls.Button;
+import openfl.events.MouseEvent;
 import feathers.controls.LayoutGroup;
 import feathers.layout.AnchorLayout;
 import feathers.layout.AnchorLayoutData;
 import feathers.skins.RectangleSkin;
 
 class BasePopup extends BaseOverlay {
-	public var contentWidth = 320.0;
-	public var contentHeight = 480.0;
-	public var content:LayoutGroup;
+	public var contentWidth = 400.0;
+	public var contentHeight = 400.0;
+
+	private var content:LayoutGroup;
 
 	override private function initialize():Void {
 		super.initialize();
@@ -18,18 +22,18 @@ class BasePopup extends BaseOverlay {
 		this.content = new LayoutGroup();
 		this.content.width = this.contentWidth;
 		this.content.height = this.contentHeight;
+		this.content.layout = new AnchorLayout();
 		this.content.layoutData = AnchorLayoutData.center();
 		this.addChild(this.content);
 
 		this.contentBackgroundFactory();
 	}
-	
-	private function contentBackgroundFactory():Void {
 
+	private function contentBackgroundFactory():Void {
 		var skin = new RectangleSkin();
-		skin.cornerRadius = 6;
+		skin.cornerRadius = 10.0;
 		skin.fill = SolidColor(0x000000);
-		skin.border = SolidColor(4, 0xFFFFFF);
+		skin.border = SolidColor(2.0, 0xFFFFFF);
 		this.content.backgroundSkin = skin;
 	}
 }
