2560817fa9cb7985ad0efe420ca1a2b4caa62413
Mansour <mansurjavadi@gmail.com>
2020-11-05 11:59:01 +0330

    Add BasePopup

---
+++ src/com/grantech/matchnums/screens/BasePopup.hx
@@ -0,0 +1,30 @@
+package com.grantech.matchnums.screens;
+
+import feathers.controls.LayoutGroup;
+import feathers.layout.AnchorLayout;
+import feathers.layout.AnchorLayoutData;
+import feathers.skins.RectangleSkin;
+
+class BasePopup extends BaseOverlay {
+	public var contentWidth = 320.0;
+	public var contentHeight = 480.0;
+	public var content:LayoutGroup;
+
+	override private function initialize():Void {
+		super.initialize();
+
+		this.layout = new AnchorLayout();
+
+		this.content = new LayoutGroup();
+		this.content.width = this.contentWidth;
+		this.content.height = this.contentHeight;
+		this.content.layoutData = AnchorLayoutData.center();
+		this.addChild(this.content);
+
+		var skin = new RectangleSkin();
+		skin.cornerRadius = 6;
+		skin.fill = SolidColor(0x000000);
+		skin.border = SolidColor(4, 0xFFFFFF);
+		this.content.backgroundSkin = skin;
+	}
+}
