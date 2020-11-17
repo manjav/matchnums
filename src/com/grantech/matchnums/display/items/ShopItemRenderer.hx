53eefe0d39ca44e3e3a1cd6754690976455b6edd
Mansour <mansurjavadi@gmail.com>
2020-11-17 12:02:27 +0330

    Add button for shop items

--- src/com/grantech/matchnums/display/items/ShopItemRenderer.hx
+++ src/com/grantech/matchnums/display/items/ShopItemRenderer.hx
@@ -1,11 +1,24 @@
 package com.grantech.matchnums.display.items;
 
+import feathers.controls.Button;
 import feathers.controls.dataRenderers.ItemRenderer;
 import openfl.Assets;
 import openfl.display.Bitmap;
 
 class ShopItemRenderer extends ItemRenderer {
+	/* override private function set_data(value:Dynamic):Dynamic {
+		if (this.data == value)
+			return value;
+		if (value == null)
+			return value;
+		// this.tool = cast(value, Tool);
+		// this.icon = new ScaledBitmap(tool.type);
+		// this.selectedIcon = new ScaledBitmap(tool.type + "-blue");
+		return super.set_data(value);
+	}*/
 	static public var SIZE:Float = 72;
+
+	private var buttonDisplay:Button;
 
 	public function new() {
 		super();
@@ -22,4 +35,18 @@ class ShopItemRenderer extends ItemRenderer {
 		super.update();
 	}
 
+	override function initialize():Void {
+		super.initialize();
+
+		this.buttonDisplay = new Button();
+		this.buttonDisplay.enabled = false;
+		this.buttonDisplay.width = 120;
+		this.addChild(this.buttonDisplay);
+	}
+
+	override private function layoutContent():Void {
+		this.buttonDisplay.x = this.actualWidth - this.paddingRight - this.buttonDisplay.width;
+		this.buttonDisplay.y = (this.actualHeight - this.buttonDisplay.height) * 0.5;
+		super.layoutContent();
+	}
 }
