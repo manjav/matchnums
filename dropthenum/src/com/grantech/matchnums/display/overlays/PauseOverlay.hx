449a4864b208ed69e89b7c92aee731936221702f
Mansour <mansurjavadi@gmail.com>
2020-12-23 12:51:18 +0330

    Change PuaseOverlay title to icon

--- dropthenum/src/com/grantech/matchnums/display/overlays/PauseOverlay.hx
+++ dropthenum/src/com/grantech/matchnums/display/overlays/PauseOverlay.hx
@@ -1,29 +1,56 @@
 package com.grantech.matchnums.display.overlays;
 
+import com.grantech.matchnums.themes.LineSkin;
 import com.grantech.matchnums.themes.OutlineTheme;
-import feathers.controls.Label;
+import feathers.controls.Button;
+import feathers.controls.LayoutGroup;
 import feathers.layout.AnchorLayout;
 import feathers.layout.AnchorLayoutData;
+import openfl.Assets;
+import openfl.display.Bitmap;
+import openfl.display.Shape;
+import openfl.events.Event;
 import openfl.events.MouseEvent;
 
 class PauseOverlay extends BaseOverlay {
-	private var labelDisplay:Label;
-
 	override private function initialize():Void {
 		super.initialize();
 
 		this.layout = new AnchorLayout();
 
-		this.labelDisplay = new Label();
-		this.labelDisplay.variant = OutlineTheme.VARIANT_LABEL_LARG;
-		this.labelDisplay.layoutData = AnchorLayoutData.center();
-		this.labelDisplay.text = "Tap to Start";
-		this.addChild(this.labelDisplay);
+		var triangle = new Shape();
+		triangle.graphics.beginFill(OutlineTheme.BORDER_COLOR, 0.1);
+		triangle.graphics.lineStyle(2.0 * OutlineTheme.SCALE_FACTOR, OutlineTheme.BORDER_COLOR);
+		var C = 8.0 * OutlineTheme.SCALE_FACTOR;
+		var W = 60.0 * OutlineTheme.SCALE_FACTOR;
+		var H = 72.0 * OutlineTheme.SCALE_FACTOR;
+		triangle.graphics.moveTo(C, 0);
+		triangle.graphics.lineTo(W * 2 - C, H - C);
+		triangle.graphics.curveTo(W * 2, H, W * 2 - C, H + C);
+		triangle.graphics.lineTo(C, H * 2);
+		triangle.graphics.curveTo(0, H * 2, 0, H * 2 - C);
+		triangle.graphics.lineTo(0, C);
+		triangle.graphics.curveTo(0, 0, C, 0);
 
-		this.addEventListener(MouseEvent.CLICK, this_clickHandler);
+		var resumeButton = new Button();
+		resumeButton.backgroundSkin = triangle;
+		resumeButton.name = "resume";
+		resumeButton.layoutData = AnchorLayoutData.center();
+		resumeButton.addEventListener(MouseEvent.CLICK, this.buttons_clickHandler);
+		this.addChild(resumeButton);
+
+	}
+		
+	private function buttons_clickHandler(event:MouseEvent):Void {
+		var button = cast(event.target, Button);
+		if (button.name == "mute" || button.name == "unmute") {
+			return;
 	}
 
-	private function this_clickHandler(event:MouseEvent):Void {
+		switch (button.name) {
+			case "resume":
+				this.dispatchEvent(new Event(Event.CANCEL));
+		};
 		this.close();
 	}
 }
