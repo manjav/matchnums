0c34cb29890399ad685e70028d0880527daf8971
Mansour <mansurjavadi@gmail.com>
2021-01-13 19:42:23 +0330

    New Cell skin

--- mergenums/src/com/dailygames/mergenums/Cell.hx
+++ mergenums/src/com/dailygames/mergenums/Cell.hx
@@ -6,7 +6,7 @@ import com.dailygames.mergenums.utils.Utils;
 import openfl.display.Shape;
 import openfl.display.Sprite;
 import openfl.events.Event;
-import openfl.filters.GlowFilter;
+import openfl.filters.DropShadowFilter;
 import openfl.text.TextField;
 import openfl.text.TextFormat;
 
@@ -22,8 +22,8 @@ class Cell extends Sprite {
 	static public final SPEED = 0.8;
 	static public final SIZE = 176;
 	static public final RADIUS = SIZE * 0.5;
-	static public final BORDER = 8;
-	static public final ROUND = 32;
+	static public final BORDER = 10;
+	static public final ROUND = 44;
 	static public final COLORS = [
 		0x000000, 0x9600ff, 0xf0145a, 0xffc91b, 0x00c419, 0x009ade, 0xce007b4, 0xff5518, 0x78e0bc, 0x3c14ae, 0xff0024, 0x41046a, 0x41046a, 0x41046a, 0x41046a
 	];
@@ -52,16 +52,14 @@ class Cell extends Sprite {
 		super();
 
 		this.background = new Shape();
-		this.background.graphics.beginFill(0xFFFFFF);
-		this.background.graphics.drawRoundRect(BORDER - RADIUS, BORDER - RADIUS, SIZE - BORDER * 2, SIZE - BORDER * 2, ROUND, ROUND);
 		this.addChild(this.background);
 
 		this.textDisplay = Utils.createText(80);
-		this.textDisplay.filters = [new GlowFilter(0, 0.6, 4, 4)];
+		this.textDisplay.filters = [new DropShadowFilter(4, 45, 0x000000, 0.3, 1.5, 1.5)];
 		this.textDisplay.width = SIZE;
 		this.textDisplay.height = RADIUS;
 		this.textDisplay.x = -RADIUS;
-		this.textDisplay.y = -RADIUS * 0.5;
+		this.textDisplay.y = -RADIUS * 0.65;
 		this.addChild(this.textDisplay);
 
 		this.textFormat = this.textDisplay.getTextFormat();
@@ -77,7 +75,7 @@ class Cell extends Sprite {
 		this.reward = reward;
 		this.state = Init;
 
-		this.background.transform.colorTransform.color = COLORS[value];
+		this.drawBackground(COLORS[value]);
 
 		this.textFormat.size = Math.round(textSize * TEXT_SCALE[value]);
 		this.textDisplay.text = Std.string(getScore(value));
@@ -105,6 +103,19 @@ class Cell extends Sprite {
 		this.state = Released;
 		if (this.hasEventListener(Event.INIT))
 			this.dispatchEvent(new Event(Event.INIT));
+	}
+
+	private function drawBackground(color:UInt):Void {
+		this.background.graphics.clear();
+		this.background.graphics.beginFill(0x000000, 0.4);
+		this.background.graphics.drawRoundRect(BORDER - RADIUS - 6, BORDER - RADIUS - 6, SIZE - BORDER * 2 + 12, SIZE - BORDER * 2 + 12, ROUND + 8, ROUND + 8);
+		this.background.graphics.endFill();
+		this.background.graphics.beginFill(color, 0.7);
+		this.background.graphics.drawRoundRect(BORDER - RADIUS, BORDER - RADIUS, SIZE - BORDER * 2, SIZE - BORDER * 2, ROUND, ROUND);
+		this.background.graphics.endFill();
+		this.background.graphics.beginFill(color);
+		this.background.graphics.drawRoundRect(BORDER - RADIUS, BORDER - RADIUS, SIZE - BORDER * 2, Math.round(SIZE - BORDER * 3.2), ROUND, ROUND);
+		this.background.graphics.endFill();
 	}
 
 	private function onDispose():Void {
