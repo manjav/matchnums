5690b136a281a99581196c6da36e301556058d33
Mansour <mansurjavadi@gmail.com>
2021-01-26 00:12:39 +0330

    Reskin starting offers

--- mergenums/src/com/dailygames/mergenums/display/overlays/StartingOfferOverlay.hx
+++ mergenums/src/com/dailygames/mergenums/display/overlays/StartingOfferOverlay.hx
@@ -1,122 +1,131 @@
 package com.dailygames.mergenums.display.overlays;
 
-import com.dailygames.mergenums.themes.OutlineTheme;
+import com.dailygames.mergenums.display.buttons.MessageButton;
+import com.dailygames.mergenums.display.popups.ConfirmPopup;
 import com.dailygames.mergenums.utils.Prefs;
 import feathers.controls.AssetLoader;
-import feathers.controls.Button;
 import feathers.controls.Label;
 import feathers.controls.LayoutGroup;
 import feathers.layout.AnchorLayout;
 import feathers.layout.AnchorLayoutData;
-import feathers.layout.VerticalLayout;
-import feathers.skins.RectangleSkin;
+import feathers.layout.ILayoutData;
 import feathers.style.Theme;
-import openfl.Assets;
-import openfl.display.Bitmap;
 import openfl.events.MouseEvent;
 
-class StartingOfferOverlay extends BaseOverlay {
-	public var hasBoostBig:Bool;
-	public var hasBoostNext:Bool;
-	private var boostBig:LayoutGroup;
-	private var boostNext:LayoutGroup;
+using com.dailygames.mergenums.themes.OutlineTheme;
+
+class StartingOfferOverlay extends ConfirmPopup {
+	public var boostBig:BoostActivator;
+	public var boostNext:BoostActivator;
 
 	override private function initialize():Void {
 		super.initialize();
 
-		this.layout = new AnchorLayout();
+		this.hasCloseButton = false;
+		this.title = "Select Boost Items";
 
-		var title = new Label();
-		title.text = "Select Boost Item";
-		title.layoutData = AnchorLayoutData.center(0, -this.padding * 8);
-		this.addChild(title);
+		this.boostBig = new BoostActivator("boost-big", "Start the game with block 512!", new AnchorLayoutData(14.I(), null, 102.I(), 14.I()));
+		this.content.addChild(this.boostBig);
+		this.boostNext = new BoostActivator("boost-next", "Preview the next upcoming block!", new AnchorLayoutData(14.I(), 14.I(), 102.I()));
+		this.content.addChild(this.boostNext);
 
-		this.boostBig = this.createBoost("boost-big", "Get 512 Block", AnchorLayoutData.center(-this.padding * 4.5));
-		this.boostNext = this.createBoost("boost-next", "Show Next Block", AnchorLayoutData.center(this.padding * 4.5));
-
-		var startButton = new Button();
-		startButton.text = startButton.name = "Start";
-		startButton.height = this.padding * 3;
-		startButton.width = this.padding * 6;
-		startButton.layoutData = AnchorLayoutData.center(0, this.padding * 8);
-		startButton.addEventListener(MouseEvent.CLICK, this.startButton_clickHandler);
-		this.addChild(startButton);
+		this.addButton("continue", "Start", OutlineTheme.VARIANT_MBUTTON_BLUE, new AnchorLayoutData(null, 14.F(), 15.F(), 14.F()));
+		this.addButton("noads", null, OutlineTheme.VARIANT_MBUTTON_ORANGE, AnchorLayoutData.bottomCenter(-90.F()), 80.I(), 80.I());
 	}
 
-	private function createBoost(icon:String, text:String, layoutData:AnchorLayoutData):LayoutGroup {
-		var skin = new RectangleSkin();
-		skin.fill = SolidColor(OutlineTheme.GRAY_COLOR, 1);
-		skin.border = SolidColor(2.0 * OutlineTheme.SCALE_FACTOR, OutlineTheme.LIGHT_COLOR);
-		skin.cornerRadius = 5.0;
+	override private function adjustContentLayout():Void {
+		this.content.width = OutlineTheme.POPUP_SIZE * 1.05;
+		this.content.height = OutlineTheme.POPUP_SIZE * 1.16;
+		this.content.layoutData = AnchorLayoutData.center();
+	}
 
-		var layout = new VerticalLayout();
-		layout.gap = layout.paddingTop = layout.paddingRight = layout.paddingBottom = layout.paddingLeft = padding * 0.4;
-		layout.horizontalAlign = JUSTIFY;
-		var boost = new LayoutGroup();
-		boost.layout = layout;
-		boost.width = this.padding * 8.5;
-		boost.layoutData = layoutData;
-		boost.backgroundSkin = skin;
-		this.addChild(boost);
-		
-		var iconDisplay = new AssetLoader();
-		iconDisplay.height = this.padding * 5;
-		boost.addChild(iconDisplay);
+	override private function titleFactory():Void {
+		this.labelFactory(this.titleDisplay, this.title, AnchorLayoutData.topCenter(-this.padding * 1.65));
+	}
+
+	override private function buttons_clickHandler(event:MouseEvent):Void {
+		var button = cast(event.currentTarget, MessageButton);
+		switch (button.name) {
+			case "continue":
+				close();
+			case "noads":
+				return;
+		}
+	}
+}
+
+class BoostActivator extends LayoutGroup {
+	public var icon:String;
+	public var text:String;
+	public var active:Bool;
+	public var coinButton:MessageButton;
+	public var adsButton:MessageButton;
+
+	public function new(icon:String, text:String, layoutData:AnchorLayoutData) {
+		super();
+		this.icon = icon;
+		this.text = text;
+		this.layoutData = layoutData;
+		this.width = OutlineTheme.POPUP_SIZE * 0.475;
+	}
+
+	override private function initialize():Void {
+		super.initialize();
 
 		var theme = cast(Theme.getTheme(), OutlineTheme);
+
+		this.layout = new AnchorLayout();
+		this.layoutData = layoutData;
+		this.backgroundSkin = theme.getButtonSkin();
+
+		var iconDisplay = new AssetLoader();
+		iconDisplay.height = 92.I();
+		iconDisplay.source = icon;
+		iconDisplay.layoutData = AnchorLayoutData.topCenter(12.I());
+		this.addChild(iconDisplay);
+
 		var textDisplay = new Label();
 		textDisplay.text = text;
-		var format = theme.getTextFormat(Math.round(OutlineTheme.FONT_SIZE * 0.8));
-		format.align = "center";
-		textDisplay.textFormat = format;
-		boost.addChild(textDisplay);
+		textDisplay.wordWrap = true;
+		textDisplay.variant = OutlineTheme.VARIANT_LABEL_DETAILS;
+		textDisplay.layoutData= new AnchorLayoutData(null, 16.I(), 120.I(), 16.I());
+		this.addChild(textDisplay);
 
-		var boostByCoin = new Button();
-		boostByCoin.height = this.padding * 2;
-		boostByCoin.name = icon;
-		boostByCoin.text = " 100 ";
-		boostByCoin.icon = new Bitmap(Assets.getBitmapData("coin"));
-		boostByCoin.addEventListener(MouseEvent.CLICK, this.coinButton_clickHandler);
-		boost.addChild(boostByCoin);
-
-		var boostByAds = new Button();
-		boostByAds.height = this.padding * 2;
-		boostByAds.name = icon;
-		boostByAds.text = " Free ";
-		boostByAds.icon = new Bitmap(Assets.getBitmapData("ads"));
-		boostByAds.addEventListener(MouseEvent.CLICK, this.adsButton_clickHandler);
-		boost.addChild(boostByAds);
-
-		return boost;
+		coinButton = addButton("coin", "100", OutlineTheme.VARIANT_MSBUTTON, new AnchorLayoutData(null, 24.I(), 68.I(), 24.I()));
+		adsButton = addButton("ads", "Free", OutlineTheme.VARIANT_MSBUTTON_ORANGE, new AnchorLayoutData(null, 24.I(), 19.I(), 24.I()));
 	}
 
-	private function startButton_clickHandler(event:MouseEvent):Void {
-		close();
+	private function addButton(name:String, message:String, variant:String, layout:ILayoutData):MessageButton {
+		var button = new MessageButton();
+		button.icon = name;
+		button.name = name;
+		button.height = 40.I();
+		button.message = message;
+		button.variant = variant;
+		button.layoutData = layout;
+		button.addEventListener(MouseEvent.CLICK, this.buttons_clickHandler);
+		this.addChild(button);
+		return button;
 	}
 
-	private function coinButton_clickHandler(event:MouseEvent):Void {
-		var button = cast(event.target, Button);
-		var newValue = Prefs.instance.get(Prefs.COIN) - 100;
-		Prefs.instance.set(Prefs.COIN, newValue);
-		if (newValue < 0)
-			return;
-		this.updateButtons(button.name);
-	}
-
-	private function adsButton_clickHandler(event:MouseEvent):Void {
-		var button = cast(event.target, Button);
-		this.updateButtons(button.name);
-	}
-
-	private function updateButtons(boostMode:String):Void {
-		if (boostMode == "boost-big") {
-			this.boostBig.mouseEnabled = false;
-			this.boostBig.alpha = 0.5;
-			this.hasBoostBig = true;
-		} else if (boostMode == "boost-next") {
-			this.boostNext.mouseEnabled = false;
-			this.boostNext.alpha = 0.5;
-			this.hasBoostNext = true;
+	private function buttons_clickHandler(event:MouseEvent):Void {
+		var button = cast(event.currentTarget, MessageButton);
+		switch (button.name) {
+			case "coin":
+				var newValue = Prefs.instance.get(Prefs.COIN) - 100;
+				Prefs.instance.set(Prefs.COIN, newValue);
+				if (newValue >= 0)
+					this.updateButtons();
+			case "ads":
+				return;
 		}
+	}
+
+	private function updateButtons():Void {
+		this.coinButton.mouseEnabled = false;
+		this.coinButton.alpha = 0.5;
+		this.adsButton.mouseEnabled = false;
+		this.adsButton.alpha = 0.5;
+		this.active = true;
 	}
 }
