db7b375aae5f97c5951465919fa0ae610ed29cfc
Mansour <mansurjavadi@gmail.com>
2021-02-03 13:24:27 +0330

    badge to iconButton

--- mergenums/src/com/dailygames/mergenums/display/buttons/IconButton.hx
+++ mergenums/src/com/dailygames/mergenums/display/buttons/IconButton.hx
@@ -1,5 +1,8 @@
 package com.dailygames.mergenums.display.buttons;
 
+import com.dailygames.mergenums.themes.OutlineTheme;
+import feathers.style.Theme;
+import feathers.controls.Label;
 import feathers.controls.AssetLoader;
 import feathers.controls.LayoutGroup;
 import feathers.layout.AnchorLayout;
@@ -8,6 +11,7 @@ import feathers.layout.AnchorLayoutData;
 class IconButton extends LayoutGroup {
 	public var padding = 0;
 
+	private var badgeDisplay:Label;
 	private var iconDisplay:AssetLoader;
 
 	private var _icon:String;
@@ -23,8 +27,26 @@ class IconButton extends LayoutGroup {
 			return this._icon;
 		this._icon = value;
 		if (iconDisplay != null)
-			iconDisplay.source = icon;
+			iconDisplay.source = this._icon;
 		return this._icon;
+	}
+
+	private var _badge:Float = Math.POSITIVE_INFINITY;
+
+	public var badge(get, set):Float;
+
+	private function get_badge():Float {
+		return this._badge;
+	}
+
+	private function set_badge(value:Float):Float {
+		if (this._badge == value)
+			return this._badge;
+		this._badge = value;
+		if (this.badgeDisplay == null)
+			this.badgeDisplay = this.badgeFactory();
+		badgeDisplay.text = this.badgeFormat(this._badge);
+		return this._badge;
 	}
 
 	override private function initialize() {
@@ -39,5 +61,21 @@ class IconButton extends LayoutGroup {
 		this.iconDisplay.source = this.icon;
 		this.iconDisplay.layoutData = AnchorLayoutData.fill(padding);
 		this.addChild(this.iconDisplay);
+	}
+
+	public dynamic function badgeFactory():Label {
+		var theme = cast(Theme.getTheme(), OutlineTheme);
+		var b = new Label();
+		b.textFormat = theme.getTextFormat(Math.round(OutlineTheme.FONT_SIZE * 0.65), OutlineTheme.LIGHT_COLOR, true);
+		b.backgroundSkin = theme.getBadgeSkin();
+		b.layoutData = AnchorLayoutData.topRight();
+		this.addChild(b);
+		return b;
+	}
+
+	public dynamic function badgeFormat(value:Float):String {
+		if (value <= 0)
+			return " free ";
+		return " " + this._badge + " ";
 	}
 }
